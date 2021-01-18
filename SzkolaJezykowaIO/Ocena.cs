888bf57c3f758a02462b421ccbff59be4b7cd588
Shinushining <luczak.roza@gmail.com>
2021-01-18 15:26:42 +0100

    Wstępna funkcjonalność klasy Ocena

---
+++ SzkolaJezykowaIO/Ocena.cs
@@ -0,0 +1,25 @@
+﻿namespace SzkolaJezykowaIO
+{
+    public class Ocena
+    {
+        public double wartosc = 1.0;
+        public double waga;
+        public string opis;
+
+        public Ocena(double wartosc, double waga, string opis)
+        {
+            this.wartosc = wartosc;
+            this.waga = waga;
+            this.opis = opis;
+        }
+
+        public void wyswietl()
+        {
+            System.Console.Write("Ocena: ", wartosc);
+            System.Console.Write(" Waga ", waga);
+            System.Console.Write(" Opis: ", opis);
+        }
+
+
+    }
+}
---
+++ SzkolaJezykowaIO/Uczen.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzkolaJezykowaIO
+{
+    public class Uczen : Osoba
+    {
+        public List<Ocena> listaOcen;
+        public Uczen(string login, string haslo, string imie,
+        string nazwisko, int id, List<Grupa> grupa = null,
+        List<Ocena> listaOcen = null) : base(login, haslo, imie, nazwisko, id, grupa)
+        { }
+
+        public void zapiszNaZajecia()
+        {
+
+        }
+
+        public void wyswietlOceny()
+        {
+            if (listaOcen == null)
+            {
+                System.Console.WriteLine("Brak ocen do wyświetlenia");
+            }
+            else
+            {
+                listaOcen.ForEach(delegate(Ocena ocena) { ocena.wyswietl(); });
+            }
+        }
+    }
+}
