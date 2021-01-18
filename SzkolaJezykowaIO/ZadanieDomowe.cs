c0355efcb258d0ab19fceb3bd65aefed697e5615
Shinushining <luczak.roza@gmail.com>
2021-01-18 16:16:01 +0100

    Oddawanie zadania domowego

--- SzkolaJezykowaIO/ZadanieDomowe.cs
+++ SzkolaJezykowaIO/ZadanieDomowe.cs
@@ -1,8 +1,20 @@
-﻿namespace SzkolaJezykowaIO
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzkolaJezykowaIO
 {
     public class ZadanieDomowe
     {
         private string tresc;
         private string tytul;
+
+        public ZadanieDomowe (string tresc, string tytul)
+        {
+            this.tresc = tresc;
+            this.tytul = tytul;
+        }
     }
 }
