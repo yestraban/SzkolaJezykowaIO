4d76a1d7cc2038e894c0385fc9c0d03d3b6d2399
yestraban <do.mrowka@gmail.com>
2021-01-18 13:59:44 +0100

    Added Admin, zaloguj methods and unit tests

---
+++ SzkolaJezykowaIO/Admin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzkolaJezykowaIO
+{
+    public class Admin
+    {
+        public string login;
+        public string haslo;
+        public List<Uczen> listaOczekujacych;
+        public List<Grupa> listaGrup;
+
+        public Admin(string login, string haslo) //konstruktor inicjujący
+        {
+            this.login = login;
+            this.haslo = haslo;
+            this.listaOczekujacych = new List<Uczen>();
+            this.listaGrup = new List<Grupa>();
+        }
+
+        public void dodajNaListe
+    }
+}
--- SzkolaJezykowaTesty/UnitTest1.cs
+++ SzkolaJezykowaTesty/UnitTest1.cs
@@ -8,12 +8,82 @@ namespace SzkolaJezykowaTesty
     public class UnitTest1
     {
         [TestMethod]
-        public void TestPanelu()
+        public void TestLogowaniaUcznia()
         {
             PanelLogowania a=new PanelLogowania();
 
-            var element = a.listaUczniow;
+            
 
+            Uczen uczen= new Uczen("Jakub2015", "qwerty", "Stanislaw", "Marek", 1);
+
+            uczen = a.zalogujUcznia("Jakub2015", "qwerty");
+
+            Assert.AreEqual(uczen.imie, "Stanislaw");
+
+        }
+        [TestMethod]
+        public void TestLogowaniaUczniaZKoncaListy()
+        {
+            PanelLogowania a = new PanelLogowania();
+
+
+
+            Uczen uczen = new Uczen("Pan_pawel", "duzybul", "Aleksander", "Wielki", 6);
+
+            uczen = a.zalogujUcznia("Pan_pawel", "duzybul");
+
+            Assert.AreEqual(uczen.imie, "Aleksander");
+
+        }
+
+        [TestMethod]
+        public void TestLogowaniaUczniaToFail()
+        {
+            PanelLogowania a = new PanelLogowania();
+
+
+
+            Uczen uczen = new Uczen("Jakub2015", "qwerty", "Stanislaw", "Marek", 1);
+
+            uczen = a.zalogujUcznia("login", "haslo");
+
+            Assert.AreEqual(uczen, null);
+
+        }
+
+        [TestMethod]
+        public void TestLogowaniaProwadzacego()
+        {
+            PanelLogowania a = new PanelLogowania();
+
+
+
+            Prowadzacy prowadzacy = new Prowadzacy("Squirtle123", "pikachu", "Grzegorz", "Adamiakowa", 1);
+
+            prowadzacy = a.zalogujProwadzacego("Squirtle123", "pikachu");
+
+            Assert.AreEqual(prowadzacy.imie, "Grzegorz");
+
+        }
+
+        [TestMethod]
+        public void TestLogowaniaAdmina()
+        {
+            PanelLogowania a = new PanelLogowania();
+
+            a.czyZalogowanyAdmin = a.zalogujAdmina("szkolajezykowa", "admin123");
+
+            Assert.IsTrue(a.czyZalogowanyAdmin);
+        }
+
+        [TestMethod]
+        public void TestLogowaniaAdminaToFail()
+        {
+            PanelLogowania a = new PanelLogowania();
+
+            a.czyZalogowanyAdmin = a.zalogujAdmina("random", "random");
+
+            Assert.IsFalse(a.czyZalogowanyAdmin);
         }
     }
 }
