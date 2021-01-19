6dd13a0805492764dc2a00ae2fa186a75e3b18fe
yestraban <do.mrowka@gmail.com>
2021-01-19 22:34:07 +0100

    Dodane menu wyboru rodzaju konta

---
+++ SzkolaJezykowaIO/PanelLogowaniaForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace SzkolaJezykowaIO
+{
+    partial class PanelLogowaniaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.comboBox1.IntegralHeight = false;
+            this.comboBox1.Location = new System.Drawing.Point(39, 48);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Wybierz rodzaj konta";
+            // 
+            // PanelLogowaniaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "PanelLogowaniaForm";
+            this.Text = "Panel Logowania";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SzkolaJezykowaIO/PanelLogowaniaForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SzkolaJezykowaIO
+{
+    public partial class PanelLogowaniaForm : Form
+    {
+        public PanelLogowaniaForm()
+        {
+            InitializeComponent();
+           // MessageBox.Show("Witaj w Szkole Językowej eMKa");
+           // MessageBox.Show("Zaloguj się na swoje konto");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] rodzajKonta =
+            {
+                "Uczeń",
+                "Prowadzący",
+                "Admin"
+            };
+            comboBox1.Items.AddRange(rodzajKonta);
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+       // comboBox1.SelectedIndex.ToString()
+
+
+    }
+}
