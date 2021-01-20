48683d19278bcfbc162847aa848251f46fdc120b
Shinushining <luczak.roza@gmail.com>
2021-01-20 13:47:13 +0100

    Refaktoryzacja PaneluProwadzacego

--- SzkolaJezykowaIO/PanelProwadzacego.Designer.cs
+++ SzkolaJezykowaIO/PanelProwadzacego.Designer.cs
@@ -35,7 +35,6 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
@@ -43,36 +42,40 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 10);
+            this.label1.Location = new System.Drawing.Point(14, 5);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 25);
+            this.label1.Size = new System.Drawing.Size(37, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Witaj, ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(130, 9);
+            this.label2.Location = new System.Drawing.Point(65, 5);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "label2";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(27, 60);
+            this.label3.Location = new System.Drawing.Point(14, 31);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(70, 25);
+            this.label3.Size = new System.Drawing.Size(35, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "label3";
             // 
             // button1
             // 
             this.button1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
-            this.button1.Location = new System.Drawing.Point(54, 258);
+            this.button1.Location = new System.Drawing.Point(27, 134);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(210, 75);
+            this.button1.Size = new System.Drawing.Size(105, 39);
             this.button1.TabIndex = 3;
             this.button1.Text = "Wyswietl ucznia";
             this.button1.UseVisualStyleBackColor = true;
@@ -80,66 +83,61 @@
             // button2
             // 
             this.button2.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
-            this.button2.Location = new System.Drawing.Point(352, 258);
+            this.button2.Location = new System.Drawing.Point(176, 134);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(196, 73);
+            this.button2.Size = new System.Drawing.Size(98, 38);
             this.button2.TabIndex = 4;
             this.button2.Text = "Dodaj ocenę";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(646, 257);
+            this.button3.Location = new System.Drawing.Point(323, 134);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(191, 74);
+            this.button3.Size = new System.Drawing.Size(96, 38);
             this.button3.TabIndex = 5;
             this.button3.Text = "Edytuj ocenę";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(938, 257);
+            this.button4.Location = new System.Drawing.Point(469, 134);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(206, 74);
+            this.button4.Size = new System.Drawing.Size(103, 38);
             this.button4.TabIndex = 6;
             this.button4.Text = "Sprawdź zadanie domowe";
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(352, 82);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(196, 33);
-            this.comboBox1.TabIndex = 7;
-            this.comboBox1.Text = "Wybierz grupę";
-            // 
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(646, 84);
+            this.comboBox2.Location = new System.Drawing.Point(176, 44);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(191, 33);
+            this.comboBox2.Size = new System.Drawing.Size(98, 21);
             this.comboBox2.TabIndex = 8;
             this.comboBox2.Text = "Wybierz ucznia";
             // 
             // comboBox3
             // 
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(938, 84);
+            this.comboBox3.Location = new System.Drawing.Point(323, 44);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(206, 33);
+            this.comboBox3.Size = new System.Drawing.Size(105, 21);
             this.comboBox3.TabIndex = 9;
             this.comboBox3.Text = "Wybierz ocenę";
             // 
             // PanelProwadzacego
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1388, 657);
+            this.ClientSize = new System.Drawing.Size(694, 342);
             this.Controls.Add(this.comboBox3);
             this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -147,6 +145,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "PanelProwadzacego";
             this.Text = "Panel Prowadzacego";
             this.ResumeLayout(false);
@@ -163,7 +162,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.ComboBox comboBox3;
     }
--- SzkolaJezykowaIO/PanelProwadzacego.cs
+++ SzkolaJezykowaIO/PanelProwadzacego.cs
@@ -30,27 +30,15 @@ namespace SzkolaJezykowaIO
             button2.Click += new EventHandler(this.button2_Click);
             button3.Click += new EventHandler(this.button3_Click);
             button4.Click += new EventHandler(this.button4_Click);
-            foreach (Grupa grupa in prowadzacy.grupa)
+            
+            
+            foreach (Uczen ucz in prowadzacy.grupa[0].listaUczniow)
             {
-                comboBox1.Items.Add(grupa.nazwa);
+                comboBox2.Items.Add(ucz.imie + " " + ucz.nazwisko);
             }
-            if (comboBox1.SelectedIndex >= 0)
+            if (comboBox2.SelectedIndex >= 0)
             {
-                foreach (Uczen ucz in prowadzacy.grupa[comboBox1.SelectedIndex].listaUczniow)
-                {
-                    comboBox2.Items.Add(ucz.imie + " " + ucz.nazwisko);
-                }
-            }
-            else
-            {
-                foreach (Uczen ucz in prowadzacy.grupa[0].listaUczniow)
-                {
-                    comboBox2.Items.Add(ucz.imie + " " + ucz.nazwisko);
-                }
-            }
-            if (comboBox2.SelectedIndex >= 0 && comboBox1.SelectedIndex >=0)
-            {
-                foreach (Ocena ocena in prowadzacy.grupa[comboBox1.SelectedIndex].listaUczniow[comboBox2.SelectedIndex].listaOcen)
+                foreach (Ocena ocena in prowadzacy.grupa[0].listaUczniow[comboBox2.SelectedIndex].listaOcen)
                 {
                     comboBox3.Items.Add(ocena.opis+": "+ocena.wartosc);
                 }
@@ -66,52 +54,63 @@ namespace SzkolaJezykowaIO
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Uczen uczenDoWyswietlenia = prowadzacy.grupa[comboBox1.SelectedIndex].listaUczniow[comboBox2.SelectedIndex];
-            string listaOcenString = "";
-            uczenDoWyswietlenia.listaOcen.ForEach(delegate (Ocena ocena)
+            if (comboBox2.SelectedIndex >= 0)
             {
-                listaOcenString += (ocena.opis + ": " + ocena.wartosc + " (waga: " + ocena.waga + ")\n");
-            });
-        
+                Uczen uczenDoWyswietlenia = prowadzacy.grupa[0].listaUczniow[comboBox2.SelectedIndex];
+                string listaOcenString = "";
+                uczenDoWyswietlenia.listaOcen.ForEach(delegate (Ocena ocena)
+                {
+                    listaOcenString += (ocena.opis + ": " + ocena.wartosc + " (waga: " + ocena.waga + ")\n");
+                });
 
-            MessageBox.Show(uczenDoWyswietlenia.imie+" " +uczenDoWyswietlenia.nazwisko+"\nid:"+ uczenDoWyswietlenia.id+"\ngrupa: "+ prowadzacy.grupa[0]+"\nOceny:\n"+listaOcenString);
+
+                MessageBox.Show(uczenDoWyswietlenia.imie + " " + uczenDoWyswietlenia.nazwisko + "\nid:" + uczenDoWyswietlenia.id + "\ngrupa: " + prowadzacy.grupa[0] + "\nOceny:\n" + listaOcenString);
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            
-            Uczen uczenDoOceny = prowadzacy.grupa[comboBox1.SelectedIndex].listaUczniow[comboBox2.SelectedIndex];
-            
-            
-            double wartosc = Convert.ToDouble(Interaction.InputBox("podaj wartość"));
-            double waga = Convert.ToDouble(Interaction.InputBox("podaj wagę"));
-            string opis = Interaction.InputBox("podaj opis oceny");
+            if (comboBox2.SelectedIndex >= 0)
+            {
+                Uczen uczenDoOceny = prowadzacy.grupa[0].listaUczniow[comboBox2.SelectedIndex];
 
-            uczenDoOceny.listaOcen.Add(new Ocena(wartosc, waga, opis));
-            MessageBox.Show("Ocena została dodana");
+
+                double wartosc = Convert.ToDouble(Interaction.InputBox("podaj wartość"));
+                double waga = Convert.ToDouble(Interaction.InputBox("podaj wagę"));
+                string opis = Interaction.InputBox("podaj opis oceny");
+
+                uczenDoOceny.listaOcen.Add(new Ocena(wartosc, waga, opis));
+                MessageBox.Show("Ocena została dodana");
+            }
         }
         private void button3_Click(object sender, EventArgs e)
         {
-            Ocena ocenaDoZmiany = prowadzacy.grupa[comboBox1.SelectedIndex].listaUczniow[comboBox2.SelectedIndex].listaOcen[comboBox3.SelectedIndex];
-            double wartosc = Convert.ToDouble(Interaction.InputBox("podaj wartość"));
-            double waga = Convert.ToDouble(Interaction.InputBox("podaj wagę"));
-            string opis = Interaction.InputBox("podaj opis oceny");
-            ocenaDoZmiany.wartosc = wartosc;
-            ocenaDoZmiany.waga = waga;
-            ocenaDoZmiany.opis = opis;
-            prowadzacy.grupa[comboBox1.SelectedIndex].listaUczniow[comboBox2.SelectedIndex].listaOcen[comboBox3.SelectedIndex] = ocenaDoZmiany;
-            MessageBox.Show("Ocena została zmieniona");
+            if (comboBox2.SelectedIndex >= 0)
+            {
+                Ocena ocenaDoZmiany = prowadzacy.grupa[0].listaUczniow[comboBox2.SelectedIndex].listaOcen[comboBox3.SelectedIndex];
+                double wartosc = Convert.ToDouble(Interaction.InputBox("podaj wartość"));
+                double waga = Convert.ToDouble(Interaction.InputBox("podaj wagę"));
+                string opis = Interaction.InputBox("podaj opis oceny");
+                ocenaDoZmiany.wartosc = wartosc;
+                ocenaDoZmiany.waga = waga;
+                ocenaDoZmiany.opis = opis;
+                prowadzacy.grupa[0].listaUczniow[comboBox2.SelectedIndex].listaOcen[comboBox3.SelectedIndex] = ocenaDoZmiany;
+                MessageBox.Show("Ocena została zmieniona");
+            }
         }
         private void button4_Click(object sender, EventArgs e)// wyświetla najnowsze zadanie
         {
-            Uczen uczenDoOceny = prowadzacy.grupa[comboBox1.SelectedIndex].listaUczniow[comboBox2.SelectedIndex];
-            if (uczenDoOceny.listaZadan.Count!=0)
+            if (comboBox2.SelectedIndex >= 0)
             {
-                ZadanieDomowe zad = uczenDoOceny.listaZadan[uczenDoOceny.listaZadan.Count - 1];
-                MessageBox.Show("Tytuł zadania: " + zad.tytul + "\nTreść: " + zad.tresc);
+                Uczen uczenDoOceny = prowadzacy.grupa[0].listaUczniow[comboBox2.SelectedIndex];
+                if (uczenDoOceny.listaZadan.Count != 0)
+                {
+                    ZadanieDomowe zad = uczenDoOceny.listaZadan[uczenDoOceny.listaZadan.Count - 1];
+                    MessageBox.Show("Tytuł zadania: " + zad.tytul + "\nTreść: " + zad.tresc);
+                }
+                else
+                    MessageBox.Show("Brak zadań do wyświetlenia");
             }
-            else
-                MessageBox.Show("Brak zadań do wyświetlenia");
         }
     }
 }
