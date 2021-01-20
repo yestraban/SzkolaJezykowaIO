8849113d24289ee066df73f24f6f07f9ba92db0f
Shinushining <luczak.roza@gmail.com>
2021-01-20 21:50:53 +0100

    PanelProwadzacy + PanelAdmin

---
+++ SzkolaJezykowaIO/EdytujGrupeForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace SzkolaJezykowaIO
+{
+
+    public partial class EdytujGrupeForm : Form
+    {
+        public PanelLogowania panel = new PanelLogowania();
+        public Admin admin;
+        public EdytujGrupeForm(Admin podajAdmina)
+        {
+            admin = podajAdmina;
+            InitializeComponent();
+            foreach (Grupa grupa in panel.listaGrup)
+            {
+                comboBox1.Items.Add(grupa.nazwa);
+            }
+            foreach (Prowadzacy prowadzacy in panel.listaProwadzacych)
+            {
+                comboBox1.Items.Add(prowadzacy.imie + " " + prowadzacy.nazwisko);
+            }
+            button1.Click += new EventHandler(this.Button1_Click);
+            button2.Click += new EventHandler(this.Button2_Click);
+            button3.Click += new EventHandler(this.Button3_Click);
+            button4.Click += new EventHandler(this.Button4_Click);
+
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex >= 0)
+            {
+                string nazwa = Interaction.InputBox("Podaj nową nazwę");
+                panel.listaGrup[comboBox1.SelectedIndex].nazwa = nazwa;
+                MessageBox.Show("Nazwa grupy została zmieniona");
+            }
+            else
+                MessageBox.Show("Nie można dokonać zmian");
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex >= 0)
+            {
+                string poziom = Interaction.InputBox("Podaj nowy poziom");
+                panel.listaGrup[comboBox1.SelectedIndex].poziomZaawansowania = poziom;
+                MessageBox.Show("Poziom grupy został zmieniony");
+            }
+            else
+                MessageBox.Show("Nie można dokonać zmian");
+        }
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex >= 0)
+            {
+                string jezyk = Interaction.InputBox("Podaj nowy język");
+                panel.listaGrup[comboBox1.SelectedIndex].jezyk = jezyk;
+                MessageBox.Show("Język grupy został zmieniony");
+            }
+            else
+                MessageBox.Show("Nie można dokonać zmian");
+        }
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex >= 0 && comboBox2.SelectedIndex >= 0)
+            {
+                panel.listaGrup[comboBox1.SelectedIndex].prowadzacy = panel.listaProwadzacych[comboBox2.SelectedIndex];
+                MessageBox.Show("Prowadzący grupy został zmieniony");
+            }
+            else
+                MessageBox.Show("Nie można dokonać zmian");
+        }
+    }
+}
---
+++ SzkolaJezykowaIO/UsunProwadzacego.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace SzkolaJezykowaIO
+{
+    partial class UsunProwadzacego
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(91, 107);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(218, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "Wybierz prowadzącego";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Usuń";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // UsunProwadzacego
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "UsunProwadzacego";
+            this.Text = "UsunProwadzacego";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
